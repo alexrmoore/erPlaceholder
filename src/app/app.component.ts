5a0f6bf901974d4e52d99df74868698889f8dc0a
Alex <alex.moore22@outlook.com>
2021-01-30 22:04:17 +0000

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Placeholder';
+
+  constructor(){}
+
+  tryagainClick(): void {
+    window.location.href = 'http://physics-escape-room.live';
+  }
 }
